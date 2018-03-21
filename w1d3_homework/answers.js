0f9bcf6740f81b4e304b29fc8d9226356bf6324c
ortizk <ortizk@live.com>
2018-03-21 16:29:59 -0700

    Commit 5 - Getting to know you answered

--- w1d3_homework/answers.js
+++ w1d3_homework/answers.js
@@ -8,14 +8,15 @@ for(let num = 0; num <= 200; num=num+2) {
 	console.log(num)
 }
 
-EXCITED KITTEN
-for(let i = 0; i < 20; i++) {
-	if ( i % 2 !== 0);
-	console.log(`${i}Love me, pet me, HHHSSSSSSS!`)
-	if ( i % 2 === 0);
-	console.log(`${i} The catnip made me do it`)
-}
-will come back to this
+// EXCITED KITTEN
+// for(let i = 0; i < 20; i++) {
+// 	if ( i % 2 !== 0);
+// 	console.log(`${i}Love me, pet me, HHHSSSSSSS!`)
+// 	if ( i % 2 === 0);
+// 	console.log(`${i} The catnip made me do it`)
+// }
+
+// will come back to this
 
 FIZZ BUZZ
 for(let i = 1; i <= 50; i++) {
@@ -30,5 +31,22 @@ for(let i = 1; i <= 50; i++) {
 	}
 	
 }
+GETTING TO KNOW YOU
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+thom[0]="Gameboy"
+console.log(thom);
+
+karolin[1]=17;
+console.log(karolin);
+
+matt[2]="Gotham City";
+console.log(matt);
+
+kristyn[2]="Brooklyn";
+console.log(kristyn);
 
 
