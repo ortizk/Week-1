0adfb1f670d02fdc447080177d989fe44ef06b8c
ortizk <ortizk@live.com>
2018-03-21 17:41:25 -0700

    Commit 8 - I loops through their closet

--- w1d3_homework/answers.js
+++ w1d3_homework/answers.js
@@ -1,89 +1,129 @@
-// EASY GOING
-// for(let num = 1; num <= 20; num++) {
-// 	console.log(num)
-// }
+EASY GOING
+for(let num = 1; num <= 20; num++) {
+	console.log(num)
+}
 
-// GET EVEN
-// for(let num = 0; num <= 200; num=num+2) {
-// 	console.log(num)
-// }
+GET EVEN
+for(let num = 0; num <= 200; num=num+2) {
+	console.log(num)
+}
 
-// EXCITED KITTEN
-// for(let i = 0; i < 20; i++) {
-// 	if ( i % 2 !== 0);
-// 	console.log(`${i}Love me, pet me, HHHSSSSSSS!`)
-// 	if ( i % 2 === 0);
-// 	console.log(`${i} The catnip made me do it`)
-// }
+EXCITED KITTEN
+for(let i = 0; i < 20; i++) {
+	if ( i % 2 !== 0);
+	console.log(`${i}Love me, pet me, HHHSSSSSSS!`)
+	if ( i % 2 === 0);
+	console.log(`${i} The catnip made me do it`)
+}
 
-// will come back to this
+will come back to this
 
-// FIZZ BUZZ
-// for(let i = 1; i <= 50; i++) {
-// 	if ( i % 15 === 0){
-// 	  console.log(`${i} FizzBuzz`)
-// 	}
-// 	else if ( i % 3 === 0){ 
-// 	  console.log(`${i} Fizz`)
-// 	}
-// 	else if (i % 5 === 0){
-// 	  console.log(`${i} Buzz`)
-// 	}
+FIZZ BUZZ
+for(let i = 1; i <= 50; i++) {
+	if ( i % 15 === 0){
+	  console.log(`${i} FizzBuzz`)
+	}
+	else if ( i % 3 === 0){ 
+	  console.log(`${i} Fizz`)
+	}
+	else if (i % 5 === 0){
+	  console.log(`${i} Buzz`)
+	}
 	
-// }
-// GETTING TO KNOW YOU
-// const thom = ["Thom", 1000, "Christchurch"]
-// const karolin = ["Karolin", 16, "New York"]
-// const kristyn = ["Kristyn", 5, "Pittsburgh"]
-// const matt = ["Matt H", 186, "Philadelphia"]
+}
+GETTING TO KNOW YOU
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
 
-// thom[0]="Gameboy"
-// console.log(thom);
+thom[0]="Gameboy"
+console.log(thom);
 
-// karolin[1]=17;
-// console.log(karolin);
+karolin[1]=17;
+console.log(karolin);
 
-// matt[2]="Gotham City";
-// console.log(matt);
+matt[2]="Gotham City";
+console.log(matt);
 
-// kristyn[2]="Brooklyn";
-// console.log(kristyn);
+kristyn[2]="Brooklyn";
+console.log(kristyn);
 
-// YELL AT THE NINJA TURTLES
+YELL AT THE NINJA TURTLES
 
-// var ninjaTurtles=["Donatello", "Leonardo", "Raphael", "Michealangelo"];
+var ninjaTurtles=["Donatello", "Leonardo", "Raphael", "Michealangelo"];
 
-// for(let i = 0; i <= ninjaTurtles.length; i++) {
-//   console.log(ninjaTurtles[i].toUpperCase());
-// }
+for(let i = 0; i <= ninjaTurtles.length; i++) {
+  console.log(ninjaTurtles[i].toUpperCase());
+}
 
-// RETURN OF THE CLOSETS
-//Alien Attire
-// var kristynsShoe = kristynsCloset.shift()
-// console.log(kristynsCloset)
+RETURN OF THE CLOSETS
+Alien Attire
+var kristynsShoe = kristynsCloset.shift()
+console.log(kristynsCloset)
 
-// thomsCloset[2].push(kristynsShoe)
-//   console.log(thomsCloset[2]);
+thomsCloset[2].push(kristynsShoe)
+  console.log(thomsCloset[2]);
 
-//Dress Us Up
-// var Outfits = [
-//   [
-//       "Marshmallow Peeps", 
-//       "Cowboy Boots", 
-//       "Yellow Knit Hat"
-//     ],[
-//       "Grey Button-up", 
-//       "Grey Jeans", 
-//       "Raybans"
-//     ],[
-//       "Right sock", 
-//       "Wool scarf", 
-//       "GA Hoodie"]
-//   ];
+Dress Us Up
+var Outfits = [
+  [
+      "Marshmallow Peeps", 
+      "Cowboy Boots", 
+      "Yellow Knit Hat"
+    ],[
+      "Grey Button-up", 
+      "Grey Jeans", 
+      "Raybans"
+    ],[
+      "Right sock", 
+      "Wool scarf", 
+      "GA Hoodie"]
+  ];
 
-// console.log("We will be wearing " + Outfits[0] + " and " + Outfits[1] + " and " + Outfits[2]);
+console.log("We will be wearing " + Outfits[0] + " and " + Outfits[1] + " and " + Outfits[2]);
 
+DIRTY LAUNDRY
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+for(let i = 0; i < kristynsCloset.length; i++) {
+  console.log("WHIRR Now washing", kristynsCloset[i]);
+}
 
+INVENTORY
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+
+console.log(thomsCloset[0])
+console.log("Thom has " + thomsCloset[0].length + " shirts")
+console.log(thomsCloset[1])
+console.log("Thom has " + thomsCloset[1].length + " pants")
+console.log(thomsCloset[2])
+console.log("Thom has " + thomsCloset[2].length + " accessories")
 
 
 
